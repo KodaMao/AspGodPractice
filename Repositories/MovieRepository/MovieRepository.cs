d636af559babf34f06a65a5672d15149c9024adf
kodamao <engrleonelfelezario812@gmail.com>
2026-04-29 16:17:30 +0800

    Create sql helper to map the sql data to c# data, to prevent crashing if changes there is changes in table

--- Repositories/MovieRepository/MovieRepository.cs
+++ Repositories/MovieRepository/MovieRepository.cs
@@ -1,11 +1,10 @@
 ﻿using AspGodPractice.Models;
+using AspGodPractice.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
 
 namespace AspGodPractice.Repositories.MovieRepository
 {
@@ -28,33 +27,12 @@ namespace AspGodPractice.Repositories.MovieRepository
                 {
                     while (reader.Read())
                     {
-                        movies.Add(MapToMovie(reader));
+                        movies.Add(SqlHelper.MapObject<Movie>(reader));
                     }
                 }
             }
 
             return movies;
-        }
-
-        /// <summary>
-        /// The Translator: Maps raw SQL columns to the C# Movie object properties.
-        /// </summary>
-        private Movie MapToMovie(SqlDataReader reader)
-        {
-            return new Movie
-            {
-                Id = Convert.ToInt32(reader["Id"]),
-                Title = reader["Title"]?.ToString(),
-                Description = reader["Description"] != DBNull.Value ? reader["Description"].ToString() : string.Empty,
-                ReleaseDate = Convert.ToDateTime(reader["ReleaseDate"]),
-                Language = reader["Language"]?.ToString(),
-                Runtime = reader["Runtime"] != DBNull.Value ? Convert.ToInt32(reader["Runtime"]) : 0,
-                Rating = reader["Rating"]?.ToString(),
-                Awards = reader["Awards"] != DBNull.Value ? Convert.ToInt32(reader["Awards"]) : 0,
-                Director = reader["Director"]?.ToString(),
-                Studio = reader["Studio"]?.ToString(),
-                Country = reader["Country"]?.ToString()
-            };
         }
     }
 }
---
+++ Utilities/SqlHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+
+
+namespace AspGodPractice.Utilities
+{
+    public static class SqlHelper
+    {
+        public static T MapObject<T>(SqlDataReader reader) where T : new()
+        {
+            T obj = new T();
+
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                string columnName = reader.GetName(i);
+
+                var prop = properties.FirstOrDefault(p =>
+                    p.Name.Equals(columnName, StringComparison.OrdinalIgnoreCase));
+
+                if (prop != null && !reader.IsDBNull(i))
+                {
+                    try
+                    {
+                        prop.SetValue(obj, reader.GetValue(i));
+                    }
+                    catch (ArgumentException)
+                    {
+      
+                    }
+                }
+            }
+            return obj;
+        }
+    }
+}
